960ccd8c6bf9affe14487d3e88d4b2d1020db90a
Ricardo Marques <rickscombr@gmail.com>
2020-11-10 02:19:41 -0300

    Iniciando o repositório com o Boilerplate padrão

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./style"
+
+const Main = ({ title = 'React Avançado', description = 'TypeScript, ReactJS, NextJS e Styled Components' }) => (
+    <S.Wrapper>
+        <S.Logo src="/img/logo.svg" alt="Imagem de um átomo com texto React Avançado ao lado." />
+        <S.Title>{ title }</S.Title>
+        <S.Description>{ description }</S.Description>
+        <S.Illustration src="/img/hero-illustration.svg" alt="Um desenvolvedor sentado em frente ao computador com uma tela cheia de códigos" />
+    </S.Wrapper>
+)
+
+export default Main
